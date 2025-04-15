075f1c9e5ce00fd1403afc791ed914e3f81172fa
Iamiran1 <vinicius.c.miranda@icloud.com>
2025-04-15 12:50:35 -0300

    data send

--- main/main.c
+++ main/main.c
@@ -11,12 +11,15 @@
 #include "mpu6050.h"
 
 #include "Fusion.h"
+#include "hardware/adc.h"
+
+
 #define SAMPLE_PERIOD (0.01f) // replace this with actual sample period
 
 const int MPU_ADDRESS = 0x68;
 const int I2C_SDA_GPIO = 4;
 const int I2C_SCL_GPIO = 5;
-
+QueueHandle_t xQueuePos;
 static void mpu6050_reset() {
     // Two byte reset. First byte register, second byte data
     // There are a load more options to set up the device in different ways that could be added here
@@ -85,28 +88,88 @@ void mpu6050_task(void *p) {
             .axis.x = acceleration[0] / 16384.0f, // Conversão para g
             .axis.y = acceleration[1] / 16384.0f,
             .axis.z = acceleration[2] / 16384.0f,
-        };      
+        };  
+        
+        typedef struct dat {
+            FusionVector posicoes;
+            int click;
+         } data;
   
         FusionAhrsUpdateNoMagnetometer(&ahrs, gyroscope, accelerometer, SAMPLE_PERIOD);
   
         const FusionEuler euler = FusionQuaternionToEuler(FusionAhrsGetQuaternion(&ahrs));
-        printf("X %0.3f, Y %0.3f, Z %0.3f\n", accelerometer.axis.x,accelerometer.axis.y,accelerometer.axis.z);
+        // printf("X %0.3f, Y %0.3f, Z %0.3f\n", accelerometer.axis.x,accelerometer.axis.y,accelerometer.axis.z);
         if(accelerometer.axis.x > 0.100){
             click = 1;
         }
         else{
             click = 0;
         }
-        printf("Click: %d\n", click);
+        // printf("Click: %d\n", click);
+        data position;
+        position.posicoes = accelerometer;
+        position.click = click;
+        xQueueSend(xQueuePos, &position, 0);
+
         vTaskDelay(pdMS_TO_TICKS(10));
         }
 }
 
+#define UART_ID uart0
+#define BAUD_RATE 115200
+#define UART_TX_PIN 0
+#define UART_RX_PIN 1
+
+void uart_task(void *p) {
+    typedef struct dat {
+        FusionVector posicoes;
+        int click;
+     } data;
+
+    data pin_data;
+
+    uart_init( UART_ID, BAUD_RATE);
+    gpio_set_function(UART_TX_PIN, UART_FUNCSEL_NUM(UART_ID, UART_TX_PIN));
+    gpio_set_function(UART_RX_PIN, UART_FUNCSEL_NUM(UART_ID, UART_RX_PIN));
+    while (1) {
+        if (xQueueReceive(xQueuePos, &pin_data, portMAX_DELAY)) {
+            
+            int axis_y = 0;
+            int val_y = pin_data.posicoes.axis.y;
+            uint8_t bytes[4];
+            bytes[0] = (uint8_t)(axis_y);          // AXIS
+            bytes[1] = (val_y >> 8) & 0xFF;                  // VAL_1 (MSB)
+            bytes[2] = val_y & 0xFF;                         // VAL_0 (LSB)
+            bytes[3] = 0xFF;                               // EOP (-1)
+            
+            uart_write_blocking(UART_ID, bytes, 4);
+            int axis_z = 1;
+            int val_z = pin_data.posicoes.axis.z;
+            bytes[0] = (uint8_t)(axis_z);          // AXIS
+            bytes[1] = (val_z >> 8) & 0xFF;                  // VAL_1 (MSB)
+            bytes[2] = val_z & 0xFF;                         // VAL_0 (LSB)
+            bytes[3] = 0xFF;     
+            
+            uart_write_blocking(UART_ID, bytes, 4);
+            int click_val = 2;
+            int val_click = pin_data.click;
+            bytes[0] = (uint8_t)(click_val);          // AXIS
+            bytes[1] = (val_click >> 8) & 0xFF;                  // VAL_1 (MSB)
+            bytes[2] = val_click & 0xFF;                         // VAL_0 (LSB)
+            bytes[3] = 0xFF;     
+            
+            uart_write_blocking(UART_ID, bytes, 4);
+            
+        }
+    }
+}
 
 int main() {
     stdio_init_all();
+    xQueuePos = xQueueCreate(32, sizeof(float));
 
     xTaskCreate(mpu6050_task, "mpu6050_Task 1", 8192, NULL, 1, NULL);
+    xTaskCreate(uart_task, "uart_task", 8192, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
